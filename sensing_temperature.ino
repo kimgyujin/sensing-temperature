8ec5910d66571592ccf0174cf464327ae640cdde
kimgyujin <37271003+kimgyujin@users.noreply.github.com>
2019-03-24 17:17:34 +0900

    Add files via upload

---
+++ sensing_temperature.ino
@@ -0,0 +1,202 @@
+// thingspeak
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define myPeriodic 58 //in sec | Thingspeak pub is 15sec
+#define ONE_WIRE_BUS 2  // DS18B20 on arduino pin2 corresponds to D4 on physical board
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+float prevTemp = 0;
+const char* server = "api.thingspeak.com";
+String apiKey ="POOSJO4YDDZZYCMT";
+const char* MY_SSID = "HelloWirelessED57"; 
+const char* MY_PWD = "1312000664";
+int sent = 0;
+void setup() {
+  Serial.begin(115200);
+  connectWifi();
+}
+
+void loop() {
+  float temp;
+  //char buffer[10];
+  DS18B20.requestTemperatures(); 
+  temp = DS18B20.getTempCByIndex(0);
+  //String tempC = dtostrf(temp, 4, 1, buffer);//handled in sendTemp()
+  Serial.print(String(sent)+" Temperature: ");
+  Serial.println(temp);
+  
+  //if (temp != prevTemp)
+  //{
+  //sendTeperatureTS(temp);
+  //prevTemp = temp;
+  //}
+
+  sendTeperatureTS(temp);
+  sendwebserver(temp);
+  int count = myPeriodic;
+  while(count--)
+  {
+    delay(1000);
+  }
+}
+
+void connectWifi()
+{
+  Serial.print("Connecting to "+*MY_SSID);
+  WiFi.begin(MY_SSID, MY_PWD);
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("Connected");
+  Serial.println("");  
+}//end connect
+
+void sendTeperatureTS(float temp)
+{  
+   WiFiClient client;
+  
+   if (client.connect(server, 80)) { // use ip 184.106.153.149 or api.thingspeak.com
+   Serial.println("WiFi Client connected ");
+   
+   String postStr = apiKey;
+   postStr += "&field1=";
+   postStr += String(temp);
+   postStr += "\r\n\r\n";
+   
+   client.print("POST /update HTTP/1.1\n");
+   client.print("Host: api.thingspeak.com\n");
+   client.print("Connection: close\n");
+   client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+   client.print("Content-Type: application/x-www-form-urlencoded\n");
+   client.print("Content-Length: ");
+   client.print(postStr.length());
+   client.print("\n\n");
+   client.print(postStr);
+   delay(1000);
+   
+   }//end if
+   sent++;
+ client.stop();
+}//end send
+
+void sendwebserver(float temp)
+{
+   WiFiClient client;
+  
+   if (client.connect("ec2-18-188-64-159.us-east-2.compute.amazonaws.com", 80)) {
+   Serial.println("webserver connected ");
+
+   String url = "/log?";
+   url += "temp=";
+   url += String(temp);
+   
+   Serial.print("Requesting URL: ");
+   Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + "ec2-18-188-64-159.us-east-2.compute.amazonaws.com" + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  }
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+}
+/*
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2 
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+const char* ssid     = "HelloWirelessED57";
+const char* password = "1312000664";
+const char* host = "maker.ifttt.com";
+String url = "/trigger/hot_temp/with/key/dgn8s6P-i08NjUuFACxCet?value1=";
+  
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  DS18B20.begin();
+}
+
+void loop() {
+  delay(60000);
+  
+  DS18B20.requestTemperatures(); 
+  float value = DS18B20.getTempCByIndex(0);
+   Serial.println(value);
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+ 
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + String(value) + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+}
+*/
