07cb0da143e89ffb25be0e98d9485072ffef34cb
kimgyujin <37271003+kimgyujin@users.noreply.github.com>
2019-03-31 22:59:44 +0900

    Add files via upload

---
+++ graph.ino
@@ -0,0 +1,88 @@
+// thingspeak
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define myPeriodic 60 //in sec | Thingspeak pub is 15sec
+#define ONE_WIRE_BUS 2  // DS18B20 on arduino pin2 corresponds to D4 on physical board
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+float prevTemp = 0;
+const char* server = "ec2-18-188-64-159.us-east-2.compute.amazonaws.com";
+String apiKey ="POOSJO4YDDZZYCMT";
+const char* MY_SSID = "HelloWirelessED57"; 
+const char* MY_PWD = "1312000664";
+int sent = 0;
+void setup() {
+  Serial.begin(115200);
+  connectWifi();
+}
+
+void loop() {
+  float temp;
+  //char buffer[10];
+  DS18B20.requestTemperatures(); 
+  temp = DS18B20.getTempCByIndex(0);
+  //String tempC = dtostrf(temp, 4, 1, buffer);//handled in sendTemp()
+  Serial.print(String(sent)+" Temperature: ");
+  Serial.println(temp);
+  
+  //if (temp != prevTemp)
+  //{
+  //sendTeperatureTS(temp);
+  //prevTemp = temp;
+  //}
+
+
+  sendmyserver(temp);
+  int count = myPeriodic;
+  while(count--)
+  {
+    delay(1000);
+  }
+}
+
+void connectWifi()
+{
+  Serial.print("Connecting to "+*MY_SSID);
+  WiFi.begin(MY_SSID, MY_PWD);
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("Connected");
+  Serial.println("");  
+}//end connect
+
+void sendmyserver(float temp)
+{
+   WiFiClient client;
+  
+   if (client.connect(server, 80)) {
+   Serial.println("webserver connected ");
+
+   String url = "/log?";
+   url += "value=";
+   url += String(temp);
+   
+   Serial.print("Requesting URL: ");
+   Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + server + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  }
+  client.stop();
+}
---
+++ graph.js
@@ -0,0 +1,86 @@
+var express = require('express')
+var app = express()
+fs = require('fs');
+mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'me',
+    password: 'mypassword',
+    database: 'tempdb'
+})
+connection.connect();
+
+function insert_sensor(value) {
+  obj = {};
+  obj.value = value;
+ 
+  var query = connection.query('insert into sensors set ?', obj, function(err, rows, cols) {
+    if (err) throw err;
+    console.log("database insertion ok= %j", obj);
+  });
+}
+
+app.get('/', function(req, res) {
+  res.end('Nice to meet you');
+});
+
+app.get('/log', function(req, res) {
+  r = req.query;
+  console.log("GET %j", r);
+
+  insert_sensor(r.value);
+  res.end('OK:' + JSON.stringify(req.query));
+});
+
+app.get('/graph', function (req, res) {
+    console.log('got app.get(graph)');
+    var html = fs.readFile('./graph.html', function (err, html) {
+    html = " "+ html
+    console.log('read file');
+
+    var qstr = 'select * from sensors ';
+    connection.query(qstr, function(err, rows, cols) {
+      if (err) throw err;
+
+      var n = rows.length;
+      var data = "";
+      var comma = "";
+      var sy = rows[0].time.getFullYear();
+      var sM = rows[0].time.getMonth()+1;
+      var sd = rows[0].time.getDate();
+      var sh = rows[0].time.getHours();
+      var sm = rows[0].time.getMinutes();
+      var ss = rows[0].time.getSeconds();
+      var ey = rows[n-1].time.getFullYear();
+      var eM = rows[n-1].time.getMonth()+1;
+      var ed = rows[n-1].time.getDate();
+      var eh = rows[n-1].time.getHours();
+      var em = rows[n-1].time.getMinutes();
+      var es = rows[n-1].time.getSeconds();
+     // res.write("start time: " + sy + "," + sm + "," + sd + "," + sh + "," + sm + "," + ss);
+     // res.write("last time: " + ey + "," + em + "," + ed + "," + eh + "," + em + "," + es);
+      for (var i=0; i< rows.length; i++) {
+         r = rows[i];
+	 //console.log(r.time.getFullYear() + "," + r.time.getMonth() + "," + r.time.getDate() + "," + r.time.getHours() + "," + r.time.getMinutes() + "," + r.time.getSeconds() + "," + r.value);
+         data += comma + "[new Date(" + r.time.getFullYear() + "," + r.time.getMonth() + "," + r.time.getDate() + "," + r.time.getHours() + "," + r.time.getMinutes() + "," + r.time.getSeconds() + "),"+ r.value + "]";
+         comma = ",";
+      }
+      var header = "data.addColumn('date', 'Date/Time');"
+      header += "data.addColumn('number', 'Temp');"
+      html = html.replace("<%HEADER%>", header);
+      html = html.replace("<%DATA%>", data);
+
+      res.writeHeader(200, {"Content-Type": "text/html"});
+      res.write(html);
+      res.write("start time: " + sy + "-" + sM + "-" + sd + " " + sh + ":" + sm + ":" + ss + "\n");
+      res.write("last time: " + ey + "-" + eM + "-" + ed + " " + eh + ":" + em + ":" + es + "\n");
+      res.end();
+    });
+  });
+})
+
+var server = app.listen(80, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('listening at http://%s:%s', host, port)
+});
